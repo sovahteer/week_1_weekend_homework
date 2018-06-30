dfe2b647e52bf40022d56ce1c4614cddb936f849
Euan <thecableton@gmail.com>
2018-06-30 13:29:54 +0100

    completed mandatory tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -38,3 +38,39 @@ def pets_by_breed(pet_shop, breed)
   end
   return results
 end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name (pet_shop, name)
+  pets =  pet_shop[:pets]
+  pet = find_pet_by_name(pet_shop, name)
+  pets.delete(pet)
+end
+
+def add_pet_to_stock (pet_shop, new_pet)
+  pet_shop[:pets] << new_pet
+end
+
+def customer_cash (customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, cash)
+  customer[:cash] -= cash
+  return customer[:cash]
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, pet)
+    customer[:pets] << pet
+end
